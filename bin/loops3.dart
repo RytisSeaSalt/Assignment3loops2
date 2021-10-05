104cc91d4e1fb9b2268e4d1be68ae9d03c68e415
Rytis Gudkov <karlmundur12345@gmail.com>
2021-10-05 11:52:51 +0000

    loops2

---
+++ bin/loops3.dart
@@ -0,0 +1,10 @@
+import 'package:loops3/loops3.dart' as loops3;
+
+void main(List<String> arguments) {
+
+  int sum = 0;
+  for(int i = 0; i <= 10; i++ )
+  {
+    sum = sum + i;
+    print(sum);
+}
---
+++ test/loops3_test.dart
@@ -0,0 +1,8 @@
+import 'package:loops3/loops3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
